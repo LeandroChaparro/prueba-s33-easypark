79703fe2abc5085473aabb0df902e1ab9662b4f1
William Ariza Salcedo <warizasalcedo@gmail.com>
2021-09-25 22:46:05 -0500

    Se crea el paquete logica y se crea la clase Usuario

---
+++ SOFTWARE/S33EPG8/src/main/java/logica/Usuario.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author elcri
+ */
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.sql.SQLException;
+import persistencia.ConexionBD;
+
+
+public class Usuario {
+    
+}
