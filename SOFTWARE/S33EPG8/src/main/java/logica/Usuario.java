f0317331d6f91e82fe94c12e54978854713d0d9a
DESKTOP-TTC89K9\NATOLOVE <ortegacm.co@gmail.com>
2021-09-26 15:36:04 -0500

    se crean metodos, obtener contacto, actualizar contacto, borrar contacto

--- SOFTWARE/S33EPG8/src/main/java/logica/Usuario.java
+++ SOFTWARE/S33EPG8/src/main/java/logica/Usuario.java
@@ -7,7 +7,7 @@ package logica;
 
 /**
  *
- * @author elcri
+ * Grupo 8 EasyPark
  */
 
 import java.sql.ResultSet;
@@ -17,5 +17,133 @@ import persistencia.ConexionBD;
 
 
 public class Usuario {
+    
+    private int    id_usuario;
+    private String nombre;
+    private String apellido;
+    private String tipo_documento;
+    private String num_documento;
+    private String tipo_vehiculo;
+    private String placa;
+    private ConexionBD conexion;    
+    
+    
+    public Usuario() {
+        this.conexion = new ConexionBD();
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTipo_documento() {
+        return tipo_documento;
+    }
+
+    public void setTipo_documento(String tipo_documento) {
+        this.tipo_documento = tipo_documento;
+    }
+
+    public String getNum_documento() {
+        return num_documento;
+    }
+
+    public void setNum_documento(String num_documento) {
+        this.num_documento = num_documento;
+    }
+
+    public String getTipo_vehiculo() {
+        return tipo_vehiculo;
+    }
+
+    public void setTipo_vehiculo(String tipo_vehiculo) {
+        this.tipo_vehiculo = tipo_vehiculo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
+    
+  public usuarios obtenerContacto() {
+
+        String sentencia = "SELECT * FROM usuarios WHERE num_documento='" + this.num_documento + "'";
+        ResultSet rs = conexion.consultarBD(sentencia);
+
+        try {
+
+            if (rs.next()) {
+
+                this.contacto_id = rs.getInt("contacto_id");
+                this.nombre = rs.getString("nombre");
+                this.apellido = rs.getString("apellido");
+                this.tipo_Identificacion = rs.getString("tipoIdentificacion");
+                
+                
+                return this;
+
+            }
+
+        } catch (SQLException ex) {
+
+            System.out.println("Error en la consulta a la bd: " + ex.getMessage());
+
+        }
+        return null;
+    }
+
+public boolean actualizarContacto(){
+
+String sentencia="UPDATE contactos SET"+this.nombre+"',apellido='"+this.apellido+"', genero='"+this.genero+"'"
+        +",tipoIdentificacion='"+this.identificacion+"'telefono='"+this.telefono+"',direccion='"+this.direccion+"'"
+        +",correo='"+this.correo+"', edad='"+this.edad+"' WHERE num_documento="+this.num_documento+";";
+return conexion.actualizarBD(sentencia);
+
+}
+
+public boolean borrarContacto(int num_documento){
+
+    String sentencia= "DELETE FROM usuarios WHERE num_documento="+num_documento;
+    return conexion.borrarBD(sentencia);
+            
+  
+}  
+    
+    
+    
     
 }
